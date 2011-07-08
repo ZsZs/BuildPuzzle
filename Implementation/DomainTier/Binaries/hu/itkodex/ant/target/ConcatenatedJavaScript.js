f55943096935d6d73b7650a3b82580553e3288c4
Zsolt Zsuffa <zsolt.zsuffa@gmail.com>
2011-07-08 09:56:58 +0200

    Adjusted to able to use Git.

---
+++ Implementation/DomainTier/Binaries/hu/itkodex/ant/target/ConcatenatedJavaScript.js
@@ -0,0 +1,24 @@
+/*** JavaScript_1.js ***/
+var Cat = new Class({
+    initialize: function(name){
+        this.name = name;
+    }
+});
+var myCat = new Cat('Micia');
+
+/*** JavaScript_2.js ***/
+var Dog = new Class({
+    initialize: function(name){
+        this.name = name;
+    }
+});
+var myDog = new Dog('Lesly');
+
+/*** JavaScript_3.js ***/
+var Horse = new Class({
+    initialize: function(name){
+        this.name = name;
+    }
+});
+var myHorse = new Horse('Tarantella');
+
